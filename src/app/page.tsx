8997648dbf77335aa75775409f5485079e7ffa02
felicitematthieu <github@matthieufelicite.com>
2024-05-13 10:17:25 +0200

    update landpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
 		<>
 
-			<MaxWidthWrapper className="flex flex-col items-center justify-center text-center min-h-[calc(100vh-56px)]">
+			<MaxWidthWrapper className="flex flex-col items-center justify-center text-center min-h-screen">
 
 				<div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md bacdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
 
