f76af7a77c771ec3dd20350c435fad22dc65c1be
felicitematthieu <github@matthieufelicite.com>
2024-05-17 22:09:10 +0200

    correct import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
